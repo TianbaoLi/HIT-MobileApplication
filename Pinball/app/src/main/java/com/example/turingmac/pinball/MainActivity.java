3e386f30ab22c756fa5474a58ee8270b9f9babb6
TuringMacLee <turingmac@hotmail.com>
2016-06-08 17:07:22 +0800

    Finish deop and reverse

--- Pinball/app/src/main/java/com/example/turingmac/pinball/MainActivity.java
+++ Pinball/app/src/main/java/com/example/turingmac/pinball/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.turingmac.pinball;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
+import android.view.Display;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.animation.AccelerateInterpolator;
@@ -9,17 +11,28 @@ import android.view.animation.Animation;
 import android.view.animation.DecelerateInterpolator;
 import android.view.animation.TranslateAnimation;
 import android.widget.ImageView;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity{
 
     private int duration = 2000;
-    private final int step = 100;
+    private final int step = 200;
+    private final int ballSize = 30;
+    private int height;
+    private int width;
     private ImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        WindowManager windowManager = getWindowManager();
+        Display display = windowManager.getDefaultDisplay();
+        DisplayMetrics metrics = new DisplayMetrics();
+        display.getMetrics(metrics);
+        width = metrics.widthPixels;
+        height = metrics.heightPixels;
 
         image = (ImageView) findViewById(R.id.imageView);
         image.setOnClickListener(new OnClickListener()
@@ -32,7 +45,7 @@ public class MainActivity extends AppCompatActivity{
                         Animation.RELATIVE_TO_PARENT, 0.0f,
                         Animation.RELATIVE_TO_PARENT, 0.0f,
                         Animation.RELATIVE_TO_PARENT, 0.0f,
-                        Animation.RELATIVE_TO_PARENT, 0.5f);
+                        Animation.RELATIVE_TO_PARENT, 0.5f * (height - 3 * ballSize - 15) / height);
                 animForward.setDuration(duration);
                 animForward.setInterpolator(new AccelerateInterpolator());
                 animForward.setAnimationListener(new myAnimationListener());
@@ -49,7 +62,7 @@ public class MainActivity extends AppCompatActivity{
             TranslateAnimation animReverse = new TranslateAnimation(
                     Animation.RELATIVE_TO_PARENT, 0.0f,
                     Animation.RELATIVE_TO_PARENT, 0.0f,
-                    Animation.RELATIVE_TO_PARENT, 0.5f,
+                    Animation.RELATIVE_TO_PARENT, 0.5f * (height - 3 * ballSize - 15) / height,
                     Animation.RELATIVE_TO_PARENT, 0.0f);
             animReverse.setDuration(duration);
             animReverse.setInterpolator(new DecelerateInterpolator());
