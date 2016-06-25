17f035759ffceccb9a775c0d54360227354e93c1
TuringMacLee <turingmac@hotmail.com>
2016-06-25 15:22:35 +0800

    Fix baiduMap

--- ProgramBox/app/src/main/java/com/example/turingmac/programbox/BaiduMapActivity.java
+++ ProgramBox/app/src/main/java/com/example/turingmac/programbox/BaiduMapActivity.java
@@ -9,9 +9,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.baidu.location.BDLocation;
 import com.baidu.location.BDLocationListener;
 import com.baidu.location.LocationClient;
 import com.baidu.location.LocationClientOption;
+import com.baidu.location.Poi;
 import com.baidu.mapapi.SDKInitializer;
 import com.baidu.mapapi.map.BaiduMap;
 import com.baidu.mapapi.map.BitmapDescriptor;
@@ -30,13 +32,15 @@ import com.baidu.mapapi.search.geocode.GeoCoder;
 import com.baidu.mapapi.search.geocode.OnGetGeoCoderResultListener;
 import com.baidu.mapapi.search.geocode.ReverseGeoCodeResult;
 
+import java.util.List;
+
 public class BaiduMapActivity extends AppCompatActivity {
 
 
     TextureMapView textureMapView = null;
     BaiduMap baiduMap = null;
     public LocationClient mLocationClient = null;
-    public BDLocationListener myListener = new com.example.turingmac.programbox.MyLocationListener();
+    public BDLocationListener myListener = new MyLocationListener();
     Button buttonLongLati = null;
     Button buttonDescription = null;
     Button buttonCurrent = null;
@@ -112,7 +116,7 @@ public class BaiduMapActivity extends AppCompatActivity {
                 baiduMap.addOverlay(option);
             }
             catch(Exception e)
-            {
+           {
 
             }
             try
@@ -125,58 +129,67 @@ public class BaiduMapActivity extends AppCompatActivity {
             }
             catch(Exception e)
             {
+                try {
+                    String s = data.getString("DES");
+                    GeoCoder geoCoder = GeoCoder.newInstance();
+                    geoCoder.setOnGetGeoCodeResultListener(onGetGeoCoderResultListener);
+                    Log.i("===========DES:", s);
+                    geoCoder.geocode(new GeoCodeOption().city("").address(s));
+                    Log.i("===========DES:", s);
+                } catch (Exception Ee) {
+
+                }
+            }
+            }
+            catch(Exception e)
+            {
 
             }
+            buttonLongLati.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // TODO Auto-generated method stub
+                    Intent intent = new Intent(BaiduMapActivity.this, InputLongLatiActivity.class);
+                    startActivity(intent);
+                }
+            });
+
+            buttonDescription.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // TODO Auto-generated method stub
+                    Intent intent = new Intent(BaiduMapActivity.this, InputDescriptionActivity.class);
+                    startActivity(intent);
+                }
+            });
+
+            buttonCurrent.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // TODO Auto-generated method stub
+                    mLocationClient.start();
+                    mLocationClient.requestLocation();
+                }
+            });
+            //Toast.makeText(MainActivity.this, "" + myListener.,Toast.LENGTH_SHORT).show();
         }
-        catch(Exception e)
-        {
 
+        @Override
+        protected void onDestroy() {
+            mLocationClient.stop();
+            super.onDestroy();
+            textureMapView.onDestroy();
         }
-        buttonLongLati.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // TODO Auto-generated method stub
-                Intent intent = new Intent(BaiduMapActivity.this, com.example.turingmac.programbox.InputLongLatiActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        buttonDescription.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // TODO Auto-generated method stub
-                Intent intent = new Intent(BaiduMapActivity.this, com.example.turingmac.programbox.InputDescriptionActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        buttonCurrent.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // TODO Auto-generated method stub
-                mLocationClient.start();
-                mLocationClient.requestLocation();
-            }
-        });
-        //Toast.makeText(MainActivity.this, "" + myListener.,Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    protected void onDestroy() {
-        mLocationClient.stop();
-        super.onDestroy();
-        textureMapView.onDestroy();
-    }
-    @Override
-    protected void onResume() {
-        super.onResume();
-        textureMapView.onResume();
-    }
-    @Override
-    protected void onPause() {
-        super.onPause();
-        textureMapView.onPause();
-    }
+        @Override
+        protected void onResume() {
+            super.onResume();
+            textureMapView.onResume();
+        }
+        @Override
+        protected void onPause() {
+            super.onPause();
+            textureMapView.onPause();
+        }
     private void initLocation(){
         LocationClientOption option = new LocationClientOption();
         option.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy
@@ -194,4 +207,77 @@ public class BaiduMapActivity extends AppCompatActivity {
         option.setEnableSimulateGps(true);//可选，默认false，设置是否需要过滤gps仿真结果，默认需要
         mLocationClient.setLocOption(option);
     }
+    public class MyLocationListener implements BDLocationListener {
+        @Override
+        public void onReceiveLocation(BDLocation location) {
+            //Receive Location
+            StringBuffer sb = new StringBuffer(256);
+            sb.append("time : ");
+            sb.append(location.getTime());
+            sb.append("\nerror code : ");
+            sb.append(location.getLocType());
+            sb.append("\nlatitude : ");
+            sb.append(location.getLatitude());
+            sb.append("\nlontitude : ");
+            sb.append(location.getLongitude());
+            sb.append("\nradius : ");
+            sb.append(location.getRadius());
+            if (location.getLocType() == BDLocation.TypeGpsLocation) {// GPS定位结果
+                    sb.append("\nspeed : ");
+                    sb.append(location.getSpeed());// 单位：公里每小时
+                    sb.append("\nsatellite : ");
+                    sb.append(location.getSatelliteNumber());
+                    sb.append("\nheight : ");
+                    sb.append(location.getAltitude());// 单位：米
+                    sb.append("\ndirection : ");
+                    sb.append(location.getDirection());// 单位度
+                    sb.append("\naddr : ");
+                    sb.append(location.getAddrStr());
+                    sb.append("\ndescribe : ");
+                    sb.append("gps定位成功");
+                        } else if (location.getLocType() == BDLocation.TypeNetWorkLocation) {// 网络定位结果
+                    sb.append("\naddr : ");
+                    sb.append(location.getAddrStr());
+                    //运营商信息
+                            sb.append("\noperationers : ");
+                    sb.append(location.getOperators());
+                    sb.append("\ndescribe : ");
+                    sb.append("网络定位成功");
+                } else if (location.getLocType() == BDLocation.TypeOffLineLocation) {// 离线定位结果
+                    sb.append("\ndescribe : ");
+                    sb.append("离线定位成功，离线定位结果也是有效的");
+                } else if (location.getLocType() == BDLocation.TypeServerError) {
+                    sb.append("\ndescribe : ");
+                    sb.append("服务端网络定位失败，可以反馈IMEI号和大体定位时间到loc-bugs@baidu.com，会有人追查原因");
+                } else if (location.getLocType() == BDLocation.TypeNetWorkException) {
+                    sb.append("\ndescribe : ");
+                    sb.append("网络不同导致定位失败，请检查网络是否通畅");
+                } else if (location.getLocType() == BDLocation.TypeCriteriaException) {
+                    sb.append("\ndescribe : ");
+                    sb.append("无法获取有效定位依据导致定位失败，一般是由于手机的原因，处于飞行模式下一般会造成这种结果，可以试着重启手机");
+                }
+            sb.append("\nlocationdescribe : ");
+            sb.append(location.getLocationDescribe());// 位置语义化信息
+            List<Poi> list = location.getPoiList();// POI数据
+            if (list != null) {
+                    sb.append("\npoilist size = : ");
+                    sb.append(list.size());
+                    for (Poi p : list) {
+                            sb.append("\npoi= : ");
+                            sb.append(p.getId() + " " + p.getName() + " " + p.getRank());
+                        }
+                }
+            Log.i("BaiduLocationApiDem", sb.toString());
+                    LatLng lat = new LatLng(location.getLatitude(), location.getLongitude());
+            MapStatusUpdate msu = MapStatusUpdateFactory.newLatLng(lat);
+            baiduMap.animateMapStatus(msu);
+            BitmapDescriptor bitmap = BitmapDescriptorFactory.fromResource(R.drawable.icon_gcoding);
+            //构建MarkerOption，用于在地图上添加Marker
+                    OverlayOptions option = new MarkerOptions()
+                            .position(lat)
+                            .icon(bitmap);
+            //在地图上添加Marker，并显示
+                    baiduMap.addOverlay(option);
+            }
+        }
 }
